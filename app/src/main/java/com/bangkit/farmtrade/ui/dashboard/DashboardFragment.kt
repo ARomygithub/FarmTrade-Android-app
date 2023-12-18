4b39651ae99193ff4668b599dee47da682d2f825
Ahmad Romy Zahran <ahmadromyzahran@gmail.com>
2023-12-18 16:23:59 +0700

    exp bottom navbar

--- app/src/main/java/com/bangkit/farmtrade/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/bangkit/farmtrade/ui/dashboard/DashboardFragment.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.bangkit.farmtrade.R
 import com.bangkit.farmtrade.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -27,10 +28,8 @@ class DashboardFragment : Fragment() {
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        binding.button.setOnClickListener {view ->
+            view.findNavController().navigate(R.id.action_navigation_dashboard_to_detailForecastFragment)
         }
         return root
     }
