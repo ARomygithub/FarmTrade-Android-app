42edc25f00ca166203eba2fe847ae213f3c7f22c
Ahmad Romy Zahran <ahmadromyzahran@gmail.com>
2023-12-20 22:34:37 +0700

    feat: beranda

---
+++ app/src/main/java/com/bangkit/farmtrade/ui/adapters/ProductAdapter.kt
@@ -0,0 +1,76 @@
+package com.bangkit.farmtrade.ui.adapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bangkit.farmtrade.databinding.ProductItemBinding
+import com.bumptech.glide.Glide
+import java.text.DecimalFormat
+import java.text.NumberFormat
+
+class ProductAdapter: ListAdapter<ProductItem, ProductAdapter.ViewHolder>(DIFF_CALLBACK) {
+    class ViewHolder(private val binding: ProductItemBinding): RecyclerView.ViewHolder(binding.root) {
+        private fun afterDiscount(price: Int): Int {
+            return (price * 0.8).toInt()
+        }
+        @SuppressLint("SetTextI18n")
+        fun bind(productItem: ProductItem) {
+            Glide.with(this.itemView.context)
+                .load(productItem.image)
+                .into(binding.productImage)
+            binding.productTitle.text = productItem.name
+            binding.producer.text = productItem.producer
+            binding.priceBefore.text = getCurrencyString(productItem.price)
+            binding.priceBefore.paintFlags = binding.priceBefore.paintFlags or android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+            binding.priceAfter.text = getCurrencyString(afterDiscount(productItem.price)) + " /kg"
+        }
+
+        private fun getCurrencyString(price: Int): String {
+            val formatter : NumberFormat = DecimalFormat("#,###")
+            return "Rp${formatter.format(price)}"
+        }
+    }
+    interface OnItemClickCallback {
+        fun onItemClicked(productItem: ProductItem)
+    }
+    private lateinit var onItemClickCallback: OnItemClickCallback
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ProductItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val productItem = getItem(position)
+        holder.bind(productItem)
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(productItem) }
+    }
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<ProductItem>() {
+            override fun areItemsTheSame(oldItem: ProductItem, newItem: ProductItem): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: ProductItem, newItem: ProductItem): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
+
+data class ProductItem(
+    val id: String,
+    val name: String,
+    val price: Int,
+    val description: String,
+    val image: String,
+    val producer: String,
+    val phoneNumber: String
+    // info penjual
+)
--- app/src/main/java/com/bangkit/farmtrade/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bangkit/farmtrade/ui/home/HomeFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bangkit.farmtrade.R
 import com.bangkit.farmtrade.databinding.FragmentHomeBinding
 import com.bangkit.farmtrade.ui.adapters.ImageAdapter
 import com.bangkit.farmtrade.ui.adapters.ImageItem
+import com.bangkit.farmtrade.ui.adapters.ProductAdapter
+import com.bangkit.farmtrade.ui.adapters.ProductItem
 import com.google.android.material.carousel.CarouselLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.UUID
@@ -53,14 +56,103 @@ class HomeFragment : Fragment() {
                 )
             )
             imageAdapter.submitList(imageList)
+
+            val productAdapter = ProductAdapter()
+//            productAdapter.setOnItemClickCallback(object: ProductAdapter.OnItemClickCallback{
+//                override fun onItemClicked(product: ProductItem) {
+//
+//                }
+//            })
+            rvProducts.adapter = productAdapter
+            rvProducts.layoutManager= LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            val productList = listOf(
+                ProductItem(
+                    "v1",
+                    "Carrot",
+                    15000,
+                    "Fresh and crunchy carrots",
+                    "https://fruit-vegi-list-api.cyclic.app/images/carrot.jpg",
+                    "Farm Fresh Produce",
+                    "+1234567890"
+                ),
+                ProductItem(
+                    "v2",
+                    "Broccoli",
+                    12000,
+                    "Nutrient-rich broccoli florets",
+                    "https://fruit-vegi-list-api.cyclic.app/images/beetroot.jpg",
+                    "Green Gardens",
+                    "+9876543210"
+                ),
+                ProductItem(
+                    "v3",
+                    "Spinach",
+                    18000,
+                    "Fresh and nutrient-packed spinach leaves",
+                    "https://fruit-vegi-list-api.cyclic.app/images/cauli-flower.jpg",
+                    "Organic Greens Farm",
+                    "+9876543210"
+                ),
+                ProductItem(
+                    "v4",
+                    "Bell Pepper",
+                    20000,
+                    "Colorful bell peppers for cooking",
+                    "https://fruit-vegi-list-api.cyclic.app/images/tomatoes.jpeg",
+                    "Rainbow Harvest",
+                    "+1234567890"
+                ),
+                ProductItem(
+                    "f1",
+                    "Apple",
+                    13000,
+                    "Sweet and juicy red apples",
+                    "https://fruit-vegi-list-api.cyclic.app/images/apple.jpg",
+                    "Orchard Delights",
+                    "+1112233445"
+                ),
+                ProductItem(
+                    "f2",
+                    "Banana",
+                    15000,
+                    "Ripe yellow bananas",
+                    "https://fruit-vegi-list-api.cyclic.app/images/banana.jpg",
+                    "Tropical Fruits Farm",
+                    "+5544332211"
+                ),
+                ProductItem(
+                    "f3",
+                    "Grapes",
+                    17000,
+                    "Juicy and sweet grapes bunch",
+                    "https://fruit-vegi-list-api.cyclic.app/images/graphes.png",
+                    "Vineyard Delights",
+                    "+9876543210"
+                ),
+                ProductItem(
+                    "f4",
+                    "Mango",
+                    19000,
+                    "Ripe and delicious mangoes",
+                    "https://fruit-vegi-list-api.cyclic.app/images/mango.jpg",
+                    "Tropical Fruits Farm",
+                    "+1112233445"
+                )
+            )
+            productAdapter.submitList(productList)
         }
         val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val adapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.lokasi_array,
+            R.layout.dropdown_item
+        )
+        binding.edLokasi.setAdapter(adapter)
     }
 
     override fun onDestroyView() {
